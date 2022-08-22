98b6a097e1dc7a730d262dcadb3ea002faac6212
salmamouri <mouriclub@gmail.com>
2022-08-22 16:27:49 +0600

    first commit

---
+++ js/select.js
@@ -0,0 +1,20 @@
+ document.getElementById('btn-1').addEventListener('click',function(){
+   console.log('btn pressed');
+ });
+ document.getElementById('btn-2').addEventListener('click',function(){
+   console.log('btn-2 pressed');
+ });
+ document.getElementById('btn-3').addEventListener('click',function(){
+   console.log('btn-3 pressed');
+ });
+ document.getElementById('btn-4').addEventListener('click',function(){
+   console.log('btn-4 pressed');
+ });
+ document.getElementById('btn-5').addEventListener('click',function(){
+   console.log('btn-5 pressed');
+ });
+ document.getElementById('btn-6').addEventListener('click',function(){
+   console.log('btn-6 pressed');
+ });
+
+    
