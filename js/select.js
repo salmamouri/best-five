3923161ba4c73f7dbccbe580053f846122d2a233
salmamouri <mouriclub@gmail.com>
2022-08-22 19:53:15 +0600

    calculation added

--- js/select.js
+++ js/select.js
@@ -1,25 +1,50 @@
+const ol = document.querySelector("ol");
+const allBtn = document.querySelectorAll(".player");
+const totalBtn = allBtn.length;
+let totalPlayer = 1;
+for (let i = 0; i < totalBtn; i++) {
+  allBtn[i].addEventListener("click", function (event) {
+    console.log(totalPlayer);
 
-const ol = document.querySelector("ol")
- const allBtn = document.querySelectorAll("button")
-const totalBtn = allBtn.length
- let totalPlayer = 0
+    // console.log(event.target.parentNode.children[0].innerText);
+    allBtn[i].disabled = true;
+    totalPlayer = totalPlayer + 1;
+    if (totalPlayer > 6) {
+      alert("You Cannot Select More Than 5");
+    } else {
+      const content = event.target.parentNode.children[0].innerText;
+      const liElement = document.createElement("li");
+      liElement.classList.add("player");
+      liElement.innerText = content;
+      ol.appendChild(liElement);
+    }
+  });
+}
 
- for(let i = 0; i < totalBtn ; i++){
-    allBtn[i].addEventListener("click",function(event){
-        console.log(event.target.parentNode.children[0].innerText);
-        allBtn[i].disabled = true
-        totalPlayer = totalPlayer + 1
-         if( totalPlayer > 5 ){
-      
-      alert("You Cannot Select More Than 5")
-         }
-         else{
-            const content = event.target.parentNode.children[0].innerText
-            const liElement = document.createElement("li");
-            liElement.innerText = content
-            ol.appendChild(liElement) 
-           
-         }
-    })
- }
+document.getElementById("calculate-btn").addEventListener("click", function () {
+  const selectedPlayer = ol.childElementCount;
+  const perPlayerCost = document.getElementById("per-player-cost").value;
+  let totalPlayerCost = selectedPlayer * perPlayerCost;
 
+  let totalPlayerExpense = document.getElementById("player-expense");
+  totalPlayerExpense.value = totalPlayerCost;
+});
+
+document.getElementById("total-btn").addEventListener("click", function () {
+  let managerCost = document.getElementById("manager-cost").value;
+  //let managerCost = parseFloat(managerCostString);
+  let coachCost = document.getElementById("coach-cost").value;
+  let totalPlayerExpense = Number(
+    document.getElementById("player-expense").value
+  );
+  console.log(typeof managerCost);
+  console.log(coachCost);
+  console.log(typeof totalPlayerExpense);
+  const totalString =
+    parseFloat(managerCost) +
+    parseFloat(coachCost) +
+    parseFloat(totalPlayerExpense);
+  const total = parseFloat(totalString);
+  const totalAmount = document.getElementById("total-amount");
+  totalAmount.innerText = total;
+});
