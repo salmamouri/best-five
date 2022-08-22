a1b3ad0bfcf3f713bf92e95c23c648b15522f09e
salmamouri <mouriclub@gmail.com>
2022-08-22 17:51:15 +0600

    more player added

--- js/select.js
+++ js/select.js
@@ -1,23 +1,25 @@
- document.getElementById('btn-1').addEventListener('click',function(){
-   console.log('btn pressed');
- });
- document.getElementById('btn-2').addEventListener('click',function(){
-   console.log('btn-2 pressed');
- });
- document.getElementById('btn-3').addEventListener('click',function(){
-   console.log('btn-3 pressed');
- });
- document.getElementById('btn-4').addEventListener('click',function(){
-   console.log('btn-4 pressed');
- });
- document.getElementById('btn-5').addEventListener('click',function(){
-   console.log('btn-5 pressed');
- });
- document.getElementById('btn-6').addEventListener('click',function(){
-   console.log('btn-6 pressed');
- });
 
+const ol = document.querySelector("ol")
  const allBtn = document.querySelectorAll("button")
+const totalBtn = allBtn.length
+ let totalPlayer = 0
 
- allBtn.forEach
-    
+ for(let i = 0; i < totalBtn ; i++){
+    allBtn[i].addEventListener("click",function(event){
+        console.log(event.target.parentNode.children[0].innerText);
+        allBtn[i].disabled = true
+        totalPlayer = totalPlayer + 1
+         if( totalPlayer > 5 ){
+      
+      alert("You Cannot Select More Than 5")
+         }
+         else{
+            const content = event.target.parentNode.children[0].innerText
+            const liElement = document.createElement("li");
+            liElement.innerText = content
+            ol.appendChild(liElement) 
+           
+         }
+    })
+ }
+
